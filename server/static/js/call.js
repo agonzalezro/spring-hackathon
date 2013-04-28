9f513e7e1194128768fc06c0979ee5919e21e376
Álex González <agonzalezro@gmail.com>
2013-04-28 12:57:32 +0100

    Changed mobile number to call

--- server/static/js/call.js
+++ server/static/js/call.js
@@ -1,7 +1,7 @@
 var call = function(number) {
   // TODO (agonzalezro): remove this for live
   // We don't want to call the police always :D
-  number = '+447761060459';
+  number = '+447449601002';
 
   $('#callModal > .modal-body > input#from').keyup(onModalInputKeyUp);
   $('#callModal > .modal-body > input#to').val(number);
