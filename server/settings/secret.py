ad409d25bf8a3332e571020244b37b8d2ff5e460
Álex González <agonzalezro@gmail.com>
2013-04-28 12:39:33 +0100

    Reading config for environment

--- server/settings/secret.py
+++ server/settings/secret.py
@@ -1,13 +1,9 @@
-import socket
+import os
 
 
 class SecretConfig(object):
-    TWILIO_ACCOUNT = 'AC7387657bfd3273c3d368260420daba86'
-    TWILIO_TOKEN = 'eb8f07c320a2ca8fc2067cef9975e889'
-    TWILIO_NUMBER = '+441702806169'
-
-    def __init__(self):
-        hostname = socket.gethostname()
-        is_local = hostname in ['Alexs-MacBook-Pro.local', 'foeric-ep01.local']
-        if not is_local:
-            self.SERVER_NAME = 'spring-hackaton.herokuapp.com'
+    # KeyError will be raised if this variables doesn't exist
+    TWILIO_ACCOUNT = os.environ['TWILIO_ACCOUNT']
+    TWILIO_TOKEN = os.environ['TWILIO_TOKEN']
+    TWILIO_NUMBER = os.environ['TWILIO_NUMBER']
+    SERVER_NAME = os.environ['SERVER_NAME']
