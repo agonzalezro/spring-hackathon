fd35186a41af278756f7bd929e65707567b5991c
eloycoto <eloy.coto@gmail.com>
2013-04-28 11:24:55 +0100

    Add support for servername

--- server/settings/secret.py
+++ server/settings/secret.py
@@ -2,4 +2,4 @@ class SecretConfig(object):
     TWILIO_ACCOUNT = 'AC7387657bfd3273c3d368260420daba86'
     TWILIO_TOKEN = 'eb8f07c320a2ca8fc2067cef9975e889'
     TWILIO_NUMBER = '+441702806169'
-    SERVER_NAME = 'http://spring-hackaton.herokuapp.com/'
+    SERVER_NAME = 'spring-hackaton.herokuapp.com'
